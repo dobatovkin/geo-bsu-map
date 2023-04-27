d3d56d09a00db432e2b40a17a65849b222a5c40b
dobatovkin <yahor.dabatoukin@mapbox.com>
2023-04-28 02:58:45 +0300

    feat: add layers switcher

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZGFiYXRvdWtpbiIsImEiOiJjbGc0NWc1NWQwYnh6M3RxZ
 // eslint-disable-next-line no-undef
 const map = new mapboxgl.Map({
   container: 'map', // container ID
-  style: 'mapbox://styles/mapbox/streets-v12', // style URL
+  style: 'mapbox://styles/dabatoukin/clgzr3dt200bs01qtgo751g7u', // style URL
   center: [27.54875, 53.89310], // starting pos[lng, lat]
   zoom: 19, // starting zoom
   pitch: 40,
@@ -74,4 +74,62 @@ map.on('load', () => { // execute after map has finished loading
       'fill-extrusion-opacity': 0.5,
     },
   });
+});
+
+// after the last frame rendered before the map enters an "idle" state.
+map.on('idle', () => {
+  // if these two layers were not added to the map, abort
+  if (!map.getLayer('geo-outside') || !map.getLayer('geo-1') || !map.getLayer('geo-2')) {
+    return;
+  };
+  
+  // enumerate ids for layers
+  const toggleableLayerIds = ['geo-1', 'geo-2', 'geo-outside'];
+  
+  for (const id of toggleableLayerIds) {
+    // skip layers that already have a button set up.
+    if (document.getElementById(id)) {
+        continue;
+    }
+
+    // Create a link.
+    const link = document.createElement('input');
+    link.id = id;
+    link.type = 'radio';
+    link.href = '#';
+    link.textContent = id;
+    link.className = 'active';
+
+    const label = document.createElement('label');
+    label.for = id;
+    // label.appendChild('1');
+
+    // Show or hide layer when the toggle is clicked.
+    link.onclick = function (e) {
+        const clickedLayer = this.textContent;
+        e.preventDefault();
+        e.stopPropagation();
+
+        const visibility = map.getLayoutProperty(
+            clickedLayer,
+            'visibility'
+        );
+
+        // Toggle layer visibility by changing the layout object's visibility property.
+        if (visibility === 'visible') {
+            map.setLayoutProperty(clickedLayer, 'visibility', 'none');
+            this.className = '';
+        } else {
+            this.className = 'active';
+            map.setLayoutProperty(
+                clickedLayer,
+                'visibility',
+                'visible'
+            );
+        }
+    };
+
+    const layers = document.getElementById('level-menu');
+    layers.appendChild(link);
+}
 });
