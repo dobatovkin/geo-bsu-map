8a038014d2152839e5fe8909bbf1fee21e760a10
dobatovkin <yahor.dabatoukin@mapbox.com>
2023-04-28 02:12:51 +0300

    feat: add 2 test layers

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ const map = new mapboxgl.Map({
 });
 
 map.on('load', () => { // execute after map has finished loading
+ 
   map.addSource('geo-src-outside', {
     type: 'geojson',
     data: 'geo-bsu.geojson',
@@ -23,10 +24,50 @@ map.on('load', () => { // execute after map has finished loading
     source: 'geo-src-outside',
     layout: {
       // make the layer visible by default.
-      visibility: 'visible',
+      visibility: 'none',
     },
     paint: {
       // get the extrusion parameters from the source properties
+      'fill-extrusion-color': ['get', 'color'],
+      'fill-extrusion-height': ['get', 'height'],
+      'fill-extrusion-base': ['get', 'base_height'],
+      'fill-extrusion-opacity': 0.5,
+    },
+  });
+
+  map.addSource('geo-src-1', { // 1st floor
+    type: 'geojson',
+    data: 'geo-1.geojson',
+  });
+  map.addLayer({
+    id: 'geo-1',
+    type: 'fill-extrusion',
+    source: 'geo-src-1',
+    layout: {
+      // disable layer by default.
+      visibility: 'visible',
+    },
+    paint: {
+      'fill-extrusion-color': ['get', 'color'],
+      'fill-extrusion-height': ['get', 'height'],
+      'fill-extrusion-base': ['get', 'base_height'],
+      'fill-extrusion-opacity': 0.5,
+    },
+  });
+
+  map.addSource('geo-src-2', { // 2nd floor
+    type: 'geojson',
+    data: 'geo-2.geojson',
+  });
+  map.addLayer({
+    id: 'geo-2',
+    type: 'fill-extrusion',
+    source: 'geo-src-2',
+    layout: {
+      // disable layer by default.
+      visibility: 'none',
+    },
+    paint: {
       'fill-extrusion-color': ['get', 'color'],
       'fill-extrusion-height': ['get', 'height'],
       'fill-extrusion-base': ['get', 'base_height'],
